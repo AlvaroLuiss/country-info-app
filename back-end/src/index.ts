1ef3aeaaecffc48c7be8b9821fbbdba4144783da
alvaroluiss <alvaro.luiscosta@hotmail.com>
2024-10-22 13:50:35 -0300

    alvaro : feat add project start-up

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,7 +1,10 @@
 import * as bodyParser from "body-parser";
 import express from "express";
+import * as swaggerUi from "swagger-ui-express";
 
+import swaggerDocs from "../swegger/swegger-output.json";
 import { env } from "./app/env";
+import routes from "./http/controllers/routes";
 export const app = express();
 
 app.use(bodyParser.json());
@@ -9,7 +12,9 @@ app.use(bodyParser.json());
 app.listen(env.PORT);
 
 // app.use('/api/v1/', routes)
+app.use("/api/v1/", routes);
 
 // app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 console.log(`Express server started on port ${env.PORT}`);
